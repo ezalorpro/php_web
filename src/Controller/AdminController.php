f7bf5e1d6f393182009ab09b8efb19dbbb710e0f
kleiver <kleiver615@gmail.com>
2020-05-22 12:20:02 -0400

    mejora del admin con controlador y custom list para editor

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Post;
+use App\Entity\ImagePost;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
 
@@ -26,15 +28,41 @@ class AdminController extends EasyAdminController
         }
     }
 
-    public function persistEntity($user)
-    {
-        $this->encodeUserPlainPassword($user);
-        parent::persistEntity($user);
+    public function persistEntity($entity)
+    {   
+        if ($entity instanceof User) {
+            $this->encodeUserPlainPassword($entity);
+        }
+        elseif($entity instanceof Post) {
+            $user = $this->getUser();
+            $entity->setUsuario($user);
+            $entity->setPostDate(new \Datetime('now'));
+            $entity->setPostModified(new \Datetime('now'));
+        }
+        
+        parent::persistEntity($entity);
     }
 
-    public function updateEntity($user)
+    public function updateEntity($entity)
     {
-        $this->encodeUserPlainPassword($user);
-        parent::updateEntity($user);
+        if ($entity instanceof User) {
+            $this->encodeUserPlainPassword($entity);
+        }
+        elseif($entity instanceof Post) {
+            $entity->setPostModified(new \Datetime('now'));
+        }
+
+        parent::updateEntity($entity);
+    }
+
+    protected function removeEntity($entity)
+    {
+        if ($entity instanceof Post) {
+            $images = $this->em->getRepository(ImagePost::class)->findBy(['post' => $entity]);
+            foreach ($images as $image) {
+                $this->em->remove($image);
+            }
+        }
+        parent::removeEntity($entity);
     }
 }
